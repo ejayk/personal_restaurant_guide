4d12089baa67450902056bdd3712aa9bc0aa94a9
ejayk <estherkim0907@gmail.com>
2020-11-28 21:15:41 -0500

    Add button on main activity screen working.

--- app/src/main/java/ca/gbc/comp3074/personalrestaurantguide/RestaurantListActivity.java
+++ app/src/main/java/ca/gbc/comp3074/personalrestaurantguide/RestaurantListActivity.java
@@ -2,8 +2,10 @@ package ca.gbc.comp3074.personalrestaurantguide;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 
 public class RestaurantListActivity extends AppCompatActivity {
@@ -12,6 +14,7 @@ public class RestaurantListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.entry_list_layout);
+
     }
 
     public void editBtnHandler(View v){
